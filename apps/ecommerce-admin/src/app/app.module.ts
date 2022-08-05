b70aac24f0f34797c5acbe796bdad92457ad74e8
Manakal <manakal@randomsoftware.net>
2022-08-05 21:59:15 +0530

    Added linting configurations

--- apps/ecommerce-admin/src/app/app.module.ts
+++ apps/ecommerce-admin/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, DashboardComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
