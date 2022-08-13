937e0e84670c70d0f41c36ef7e0ab01955310fff
Manakal <manakal@randomsoftware.net>
2022-08-13 20:37:32 +0530

    feat: :sparkles: Admin side-bar created

--- apps/ecommerce-admin/src/app/app.module.ts
+++ apps/ecommerce-admin/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+    ],
+  },
+];
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, DashboardComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    DashboardComponent,
+    ShellComponent,
+    SidebarComponent,
+  ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
